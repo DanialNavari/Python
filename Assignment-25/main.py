ab3e8e0c9d8edf8469d7358934e3927f722809c6
DanielNavari <eng.navari@gmail.com>
2024-06-15 09:21:51 +0330

    doning assignment 25

--- Assignment-25/main.py
+++ Assignment-25/main.py
@@ -1,8 +1,8 @@
 import sys
 import time
-from PySide6.QtWidgets import *
+from PySide6.QtCore import QObject, QThread, Signal
 from PySide6.QtGui import *
-from PySide6.QtCore import QThread
+from PySide6.QtWidgets import *
 from main_window import Ui_MainWindow
 
 
@@ -11,10 +11,11 @@ class UI(QMainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.btn_start_stopwatch.clicked.connect(MyThread.start_stopwatch)
 
 
 class MyThread(QThread):
+    signal_dani = Signal(int)
+
     def __init__(self):
         super().__init__()
         self.second = 0
@@ -22,16 +23,36 @@ class MyThread(QThread):
     def run(self):
         while True:
             self.second += 1
-            print(self.second)
+            self.signal_dani.emit(self.second)
             time.sleep(1)
 
-    def start_stopwatch(self):
-        thread_stopwatch.start()
+    def reset(self):
+        self.second = 0
 
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = UI()
-    window.show()
-    thread_stopwatch = MyThread()
-    app.exec()
+def show_number(s):
+    window.ui.lbl_stopwatch.setText(str(s))
+
+
+def start_stopwatch():
+    thread_stopwatch.start()
+
+
+def stop_stopwatch():
+    thread_stopwatch.terminate()
+
+
+def reset_stopwatch():
+    thread_stopwatch.reset()
+    show_number(0)
+
+
+app = QApplication(sys.argv)
+window = UI()
+window.show()
+thread_stopwatch = MyThread()
+thread_stopwatch.signal_dani.connect(show_number)
+window.ui.btn_start_stopwatch.clicked.connect(start_stopwatch)
+window.ui.btn_stop_stopwatch.clicked.connect(stop_stopwatch)
+window.ui.btn_reset_stopwatch.clicked.connect(reset_stopwatch)
+app.exec()
