b77861cbb932fd49a716f013821f5d0c59aef7c5
DanielNavari <eng.navari@gmail.com>
2024-06-17 23:20:42 +0330

    doing assignmen25 - complete global time

--- Assignment-25/global_time.py
+++ Assignment-25/global_time.py
@@ -5,34 +5,34 @@ from pytz import *
 
 
 class GlobalTime(QThread):
-    signal_show_country = Signal(str, str, str)
+    signal_show_country = Signal(str, str, str, str, str, str)
 
     def __init__(self):
         super().__init__()
-        self.local_time = datetime.datetime.now()
-        self.fmt = "%H:%M:%S"
-        self.zone = "%Z"
-        self.ir = timezone("Asia/Tehran")
-        self.us = timezone("US/Eastern")
-        self.de = timezone("Europe/Berlin")
-        self.ir_time = self.local_time.strftime(self.fmt)
 
     def run(self):
         while True:
-            iran_time = self.ir_time
-            us_time = self.country_time(self.us)
-            de_time = self.country_time(self.de)
-            self.signal_show_country.emit(iran_time, us_time, de_time)
+            self.local_time = datetime.datetime.now()
+            self.fmt = "%H:%M:%S"
+            self.zone = "%z"
+            self.ir = timezone("Asia/Tehran")
+            self.us = timezone("US/Eastern")
+            self.de = timezone("Europe/Berlin")
+            self.ir_time = self.local_time.strftime(self.fmt)
+            iran_time = str(self.ir_time)
+            us_time = str(self.country_time(self.us))
+            de_time = str(self.country_time(self.de))
+
+            iran_zone = str(self.country_zone(self.ir))
+            us_zone = str(self.country_zone(self.us))
+            de_zone = str(self.country_zone(self.de))
+            self.signal_show_country.emit(
+                iran_time, us_time, de_time, iran_zone, us_zone, de_zone
+            )
             time.sleep(1)
 
     def country_time(self, country):
-        country_time = (
-            utc.localize(self.local_time).astimezone(country).strftime(self.fmt)
-        )
-        return country_time
+        return utc.localize(self.local_time).astimezone(country).strftime(self.fmt)
 
     def country_zone(self, country):
-        country_zone = (
-            utc.localize(self.local_time).astimezone(country).strftime(self.zone)
-        )
-        return country_zone
+        return utc.localize(self.local_time).astimezone(country).strftime(self.zone)
--- Assignment-25/main.py
+++ Assignment-25/main.py
@@ -102,14 +102,21 @@ window.ui.btn_reset_timer.clicked.connect(reset_timer)
 
 ########################## End of timer ##########################
 
+
 ##########################  Start of Global Time ##########################
-def show_global_time(ir_time, us_time, de_time):
+def show_global_time(ir_time, us_time, de_time, ir_zone, us_zone, de_zone):
     window.ui.ir_time.setText(ir_time)
     window.ui.usa_time.setText(us_time)
     window.ui.de_time.setText(de_time)
+    window.ui.ir_timezone.setText(ir_zone)
+    window.ui.us_timezone.setText(us_zone)
+    window.ui.de_timezone.setText(de_zone)
+    print(ir_time)
+
 
 global_country_time = GlobalTime()
 global_country_time.signal_show_country.connect(show_global_time)
+global_country_time.start()
 
 
 ##########################  End of Global Time ##########################
