bc630ae99a810be906584bd2bb059f0edb9ab2da
DanialNavari <eng.navari@gmail.com>
2024-10-03 01:53:51 +0330

    working on assignment 26

--- Assignment-26/main.py
+++ Assignment-26/main.py
@@ -2,5 +2,6 @@ import cv2
 
 my_image = cv2.imread("image.jpg")
 my_image_2 = cv2.cvtColor(my_image, cv2.COLOR_RGB2GRAY)
-
-print(my_image_2.shape)
+cv2.imshow("", my_image_2)
+cv2.waitKey()
+cv2.imwrite("result.jpg", my_image_2)
