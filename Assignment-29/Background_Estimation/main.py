203128644bb8c89c6744c15a41a250214082cdb7
DanialNavari <eng.navari@gmail.com>
2024-11-16 01:56:15 +0330

    complete assignment 29

--- Assignment-29/Background_Estimation/main.py
+++ Assignment-29/Background_Estimation/main.py
@@ -1,19 +1,16 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture('Background_Estimation/input/cars.mp4')
- 
+cap = cv2.VideoCapture("Background_Estimation/input/cars.mp4")
+
 frameIds = cap.get(cv2.CAP_PROP_FRAME_COUNT) * np.random.uniform(size=25)
- 
 
 frames = []
 for fid in frameIds:
     cap.set(cv2.CAP_PROP_POS_FRAMES, fid)
     ret, frame = cap.read()
     frames.append(frame)
- 
 
-medianFrame = np.average(frames,axis=0).astype(dtype=np.uint8)    
-
+medianFrame = np.average(frames, axis=0).astype(dtype=np.uint8)
 
 cv2.imwrite("Background_Estimation/output/cars.jpg", medianFrame)
