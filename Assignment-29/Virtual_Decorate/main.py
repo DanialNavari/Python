f5caeb389ec020af46872804dd1f39e5090f3cdc
DanialNavari <eng.navari@gmail.com>
2024-11-16 00:56:00 +0330

    complete assignment 29

---
+++ Assignment-29/Virtual_Decorate/main.py
@@ -0,0 +1,26 @@
+# import library
+import cv2
+
+# read images
+home = cv2.imread("Virtual_Decorate/input/home.jpg")
+decore = cv2.imread("Virtual_Decorate/input/decore.jpg")
+mask = cv2.imread("Virtual_Decorate/input/mask.jpg")
+
+# resize home to decore shape
+home_ = cv2.resize(home, (decore.shape[1], decore.shape[0]))
+
+mask_ = mask.astype("float32")
+
+# set decore on floar
+result = mask_ / 255 * decore
+
+# convert mask colors
+mask_ = 255 - mask_
+
+# set home appliance on converted mask
+result += mask_ / 255 * home_
+
+result = result.astype("uint8")
+
+# save image
+cv2.imwrite("Virtual_Decorate/output/result.jpg", result)
--- Assignment-29/test/test.py
+++
@@ -1,5 +0,0 @@
-import turtle
-t = turtle.Turtle()
-t.dot(20)
-
-turtle.done()
